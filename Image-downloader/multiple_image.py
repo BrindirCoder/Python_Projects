e9af2f7c96986cd8b4a8a381e831205f5acdcd82
BrindirCoder <168096234+BrindirCoder@users.noreply.github.com>
2025-10-22 13:41:31 +0300

    Fix syntax error in multiple_image.py

--- Image-downloader/multiple_image.py
+++ Image-downloader/multiple_image.py
@@ -1,3 +1,5 @@
+import requests
+
 url = (
     "https://i.pinimg.com/736x/67/b7/5e/67b75e913fd5edee102a3292298c0552.jpg",
     "https://i.pinimg.com/736x/d8/52/8a/d8528a4a64dcacb72f3b4ac969d00c29.jpg",
@@ -9,4 +11,4 @@ for img in url:
     x = img.split("/")[-1]
     data = requests.get(img)
     with open(x, "wb") as file:
-        file.write(data.content
+        file.write(data.content)
